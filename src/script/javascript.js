bc68d7bb7a186ea71ccca44c9d0997ce4d78c498
Ayanami016 <kmasi4@gmail.com>
2024-06-13 12:36:43 -0300

    👨‍💻 Functions em campos

--- src/script/javascript.js
+++ src/script/javascript.js
@@ -67,8 +67,53 @@ campoTel.addEventListener("keypress", ()=>{
     }
 })
 
+//VALIDAÇÃO DOS CAMPOS
+//Todos os "vômitos de caracretes" são expressões regulares (regex) para validação dos campos
+    //Nome
+    function validarNome(nome) {
+        const verificar = /^[a-zA-Zà-úÀ-Ú'\-\s]+$/;
+        return verificar.test(nome);
+    }
+    //Email
+    function validarEmail(email) {
+        // Define a expressão regular para validação de email
+        const verificar = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return verificar.test(email);
+    }
+    //CPF
+    //Apenas validamos a quantia de caracteres
+    function validarCPF(cpf) {
+        cpf = cpf.replace(/[^\d]+/g, '');
+
+        //Deve ter 14 caracteres
+        if (cpf.length === 14) {return false;};
+        //Deve ter caracteres diferentes
+        if (/^(\d)\1+$/.test(cpf)) {return false;};
+
+        return true;
+    }
+    //Telefone
+    function validarTelefone(tel) {
+        const verificar = /^\(\d{2}\) \d{4,5}-\d{4}$/;
+        return verificar.test(tel);
+    }
+    //Confirmar Senha
+    function validarSenha() {
+        let senha = document.getElementById("senha");
+        let confirmarsenha = document.getElementById("");
+
+        let senhaValor = senha.value.trim();
+        let confirmarSenhaValor = confirmarSenha.value.trim();
+
+        if (senhaValor === confirmarSenhaValor) {
+            console.log("correto")
+        } else {
+            console.log("incoreto")
+        }
+    }
+
 //Ativa o menu mobile
 document.getElementById('menu-mobile').addEventListener('click', function() {
-    var menupc = document.querySelector('#menu-pc');
-    menupc.classList.toggle('active');
+    var menupc = document.querySelector('#menu-pc')
+    menupc.classList.toggle('active')
 });
